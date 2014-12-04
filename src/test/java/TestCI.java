34b5c168e9a87d1ac6b46dd5ae248ecebe2d6ff1
divyendu12 <divyendu12@gmail.com>
2014-12-04 15:11:51 +0530

    intialupload

---
+++ src/test/java/TestCI.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+public class TestCI {
+
+	@Test
+	 public void FirstTest (){
+	  WebDriver driver =new FirefoxDriver();
+	    String baseUrl = "http://en.wikipedia.org/wiki/Main_Page";
+	    driver.get(baseUrl);
+	    driver.findElement(By.xpath(".//*[@id='searchInput']")).sendKeys("software testing");
+	    driver.findElement(By.xpath("//*[@id='searchButton']")).click();
+	    //driver.close();
+	
+	}
+
+}
